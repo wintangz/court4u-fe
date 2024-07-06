0b00b272df66a4d25c5fcc1fd0460afb2ac04c7d
wintangz <wintang2003@icloud.com>
2024-07-06 17:06:27 +0700

    fix clubService

--- app/_services/clubService.ts
+++ app/_services/clubService.ts
@@ -1,6 +1,7 @@
 import { axiosGet, axiosPost } from './baseService';
 
 export const getClubs = () => axiosGet('/clubs', {});
+export const getClub = (id: string) => axiosGet(`/clubs/${id}`, {});
 export const getClubById = (id: string) => axiosGet(`/clubs/${id}`, {});
 export const createdClub = (data: any) => axiosPost('/clubs', data, '');
 export const updateClub = (id: string, data: any) =>
