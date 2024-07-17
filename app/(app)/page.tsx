e34f311f7b1116c73d80211603e92da5dcd8f7ab
N4mFeng <namphong24072003@gmail.com>
2024-07-18 00:02:03 +0700

    add about page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,9 +1,91 @@
+import { dot } from 'node:test/reporters';
 import Banner from '../_components/banner/Banner';
+import { url } from 'node:inspector';
+import Image from 'next/image';
+import { Router } from 'next/router';
+
+
 
 export default function Home() {
   return (
     <div className=''>
       <Banner />
+      
+      <div className="flex flex-col items-center">
+        
+        <div className="flex flex-col md:flex-row items-center p-6 w-2/3">
+          <div className="md:w-1/2 p-4">
+            <h2 className="text-2xl md:text-4xl font-bold leading-none">Booking a
+              <br />
+              <span className="text-red-600">Badminton Court</span></h2>
+
+            <div className="my-4 border-t border-gray-800"></div>
+            <div className="mb-4">
+              <h3 className="text-xl md:text-2xl font-semibold">
+                What and How <span className="text-red-600">We Serve</span>
+              </h3>
+            </div>
+
+            <div className="mb-4" >
+              <ul className="list-disc pl-5 mt-4 space-y-2">
+                <li>Court Locations</li>
+                <li>Different types of courts</li>
+                <li>Corporate Booking</li>
+                <li>Total 36 badminton courts</li>
+                <li>Training grounds</li>
+              </ul>
+            </div>
+          </div>
+          <div className='md:w-1/2 p-4 '>
+            <img
+              className="w-[680px] h-[auto]"
+              alt='court1'
+              src='../assets/court1.png'
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row items-center p-6 w-2/3">
+
+          <div className='md:w-1/2 p-2 '>
+            <img
+              className="w-[560px] h-[auto]"
+              alt='court2'
+              src='../assets/court2.png'
+            />
+          </div>
+
+          <div className="md:w-1/2 p-2">
+            <h2 className="text-2xl font-bold">Custom Booking for <br />
+              <span className="text-red-600">Trainings / Events / Competition</span></h2>
+            <p className="mt-4">Contact us directly to make a custom booking.</p>
+            <div className="mt-4 space-x-4">
+              <button className="bg-red-600 text-white py-2 px-4 rounded hover:shadow-[0_1px_0px_rgb(0,0,0)]" >Call us</button>
+              <button className="bg-red-600 text-white py-2 px-4 rounded hover:shadow-[0_1px_0px_rgb(0,0,0)]" >Email us</button>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="flex flex-col md:flex-col items-center p-6 w-1080 bg-slate-500">
+
+          <div className="md:w-1/2 p-4">
+            <h2 className="text-2xl text-center font-bold text-white">Our Badminton Courts</h2>
+          </div>
+
+          <div className="md:w-full p-4 ">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <img src="../assets/court3.png" className="h-full object-cover w-[930px]" alt="Court 3" />
+              <img src="../assets/court4.png" className="h-full object-cover w-[930px]" alt="Court 4" />
+              <img src="../assets/court5.png" className="h-full object-cover w-[930px]" alt="Court 5" />
+            </div>
+          </div>
+        </div>
+
+
+      </div>
     </div>
+    
   );
+  
 }
