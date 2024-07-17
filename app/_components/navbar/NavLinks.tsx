6d8d6136cd731acb3c9d9016e18002bb61d5ac43
wintangz <wintang2003@icloud.com>
2024-07-18 05:05:48 +0700

    update navbar

--- app/_components/navbar/NavLinks.tsx
+++ app/_components/navbar/NavLinks.tsx
@@ -4,13 +4,30 @@ import classNames from 'classnames';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { logout } from '@/app/_services/userService';
+import { jwtDecode } from 'jwt-decode';
+import { FiChevronDown } from 'react-icons/fi'; // Example: Use appropriate icon from react-icons library
 
 const NavLinks = () => {
   const currentPath = usePathname();
   const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   useEffect(() => {
     setAuthenticated(isAuthenticated());
+  }, []);
+
+  useEffect(() => {
+    if (
+      typeof window !== 'undefined' &&
+      localStorage.getItem('accessToken') !== undefined
+    ) {
+      const decodedToken: any = jwtDecode(
+        localStorage.getItem('accessToken') as string
+      );
+      console.log(decodedToken);
+      setUser(decodedToken.email.slice(0, decodedToken.email.indexOf('@')));
+    }
   }, []);
 
   const leftLinks = [
@@ -23,11 +40,10 @@ const NavLinks = () => {
     { label: 'Login', href: '/login' },
     { label: 'Register', href: '/register' },
   ];
-  const rightPrivateLinks = [{ label: 'Profile', href: '/profile' }];
 
   const isAuthenticated = () => {
     const token = localStorage.getItem('accessToken');
-    return token !== null && token != undefined; // corrected condition for checking token
+    return token !== null && token !== undefined; // corrected condition for checking token
   };
 
   const handleLogout = () => {
@@ -35,6 +51,10 @@ const NavLinks = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
     setAuthenticated(false); // Update state to rerender
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
   };
 
   return (
@@ -88,16 +108,30 @@ const NavLinks = () => {
             </li>
           ))
         ) : (
-          <div>
-            <li>Welcome, </li>
-            <li>
-              <button
-                onClick={handleLogout}
-                className='text-secondary font-bold hover:text-primary transition-colors'
-              >
-                Log out
-              </button>
-            </li>
+          <div className='relative'>
+            <button
+              onClick={toggleDropdown}
+              className='flex items-center text-secondary hover:text-primary transition-colors cursor-pointer'
+            >
+              <span className='mr-1'>Welcome, {user}</span>
+              <FiChevronDown />
+            </button>
+            {showDropdown && (
+              <div className='absolute right-0 mt-2 w-48 bg-gray-500 shadow-lg rounded-lg p-2'>
+                <Link
+                  href='/profile'
+                  className='text-sm block px-4 py-2 text-white hover:bg-gray-700'
+                >
+                  Profile
+                </Link>
+                <button
+                  onClick={handleLogout}
+                  className='text-sm block w-full text-left px-4 py-2 text-white hover:bg-gray-700'
+                >
+                  Logout
+                </button>
+              </div>
+            )}
           </div>
         )}
       </ul>
