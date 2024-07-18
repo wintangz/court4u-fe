e4de5f1a100f9b4b781e3a05ff229b2ba1f7d321
sonukik <son293526@gmail.com>
2024-07-18 15:49:03 +0700

    Upload ABOUT PAGE

--- app/(app)/about/page.tsx
+++ app/(app)/about/page.tsx
@@ -12,23 +12,28 @@ const About = () => {
           </ul>
         </nav>
       </header>
-      <main className="flex-grow p-8 bg-gray-100">
-        <section className="mb-12 p-6 border-2 border-black bg-white">
-          <h1 className="text-4xl font-bold mb-1">About Us</h1>
-          <p className="text-lg font-normal mb-1 ">
-            Welcome to Court4U! We are dedicated to providing the best court booking experience for our users.
-            Our mission is to simplify the process of finding and booking sports courts. Whether you're a casual player looking for a game, a club manager scheduling tournaments, or a coach arranging training sessions, our platform is designed to meet your needs.
-            Founded in 2024, we have quickly grown to become a trusted resource for sports enthusiasts everywhere. Our commitment to excellence and customer satisfaction is at the heart of everything we do.
-          </p>
-          <div className='relative min-h-screen'>
-            <img src="https://badmintonw.com/uploads/images/san-cau-long-dai-hoc-su-pham-ha-noi.png" alt="San Cau Long" className="absolute inset-0 w-full h-full object-cover z-0" />
-            <div className='absolute inset-0 flex justify-center items-center bg-black bg-opacity-50'>
-              <div className='flex items-center space-x-6 p-4 border-2 border-gray-300 bg-white shadow-lg'>
+      <main className="flex-grow p-8 bg-gray-100 relative">
+        <img src="https://badmintonw.com/uploads/images/san-cau-long-dai-hoc-su-pham-ha-noi.png" alt="San Cau Long" className="absolute inset-0 w-full h-full object-cover z-0" />
+        <section className="relative z-10 flex flex-col items-center justify-center min-h-screen">
+          <div className="relative z-20 p-6 bg-black bg-opacity-70 rounded text-center">
+            <h1 className="text-4xl font-bold mb-4 text-white">About Us</h1>
+            <p className="text-lg font-normal mb-1 text-white">
+              Welcome to Court4U! We are dedicated to providing the best court booking experience for our users.
+              Our mission is to simplify the process of finding and booking sports courts. Whether you're a casual player looking for a game, a club manager scheduling tournaments, or a coach arranging training sessions, our platform is designed to meet your needs.
+              Founded in 2024, we have quickly grown to become a trusted resource for sports enthusiasts everywhere.
+            </p>
+          </div>
+          <div className="relative z-20 mt-8 w-full">
+            <div className="p-6 bg-red-700 rounded-lg w-full flex flex-col justify-center items-center relative">
+              <div className="text-4xl font-bold text-white text-center py-2">
+                Book Your Court Now
+              </div>
+              <div className="relative flex items-center space-x-6 p-2 mt-2 bg-white shadow-lg rounded-lg">
                 <Link
                   href='/login'
-                  className='flex items-center space-x-6 text-3xl hover:bg-red-700 p-2 rounded'
+                  className='flex items-center space-x-6 text-lg text-black hover:bg-red-300  p-2 rounded'
                 >
-                  Book Your Court Now
+                  Choose Your Courts
                 </Link>
               </div>
             </div>
